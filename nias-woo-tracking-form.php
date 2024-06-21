7e76b8a47e49447bb038d24f8eed6dbabaf5276d
Alireza Aliniya (Nias) <100364822+Alirezaaliniya@users.noreply.github.com>
2024-06-21 12:42:32 +0330

    Create nias-woo-tracking-form.php

---
+++ nias-woo-tracking-form.php
@@ -0,0 +1,14 @@
+/*
+ * Plugin Name:       nias woo tracking form
+ * Plugin URI:        https://Nias.ir
+ * Description:       اصلاح فرم پیگیری سفارشات ووکامرس
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            علیرضا علی تیا
+ * Author URI:        https://Nias.ir/
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Update URI:        https://Nias.ir/
+ * Text Domain:       nias-woo-tracking-form
+ */
